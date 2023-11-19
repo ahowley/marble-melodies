a7361c6537822e7550724b6ec97774f1f60a44dd
Alex Howley <howley.alex@gmail.com>
2023-11-19 15:16:15 -0600

    Port lerp function from capstone sketch

--- src/game/common.ts
+++ src/game/common.ts
@@ -1,2 +1,5 @@
 export const degToRad = (degrees: number) => (degrees * Math.PI) / 180;
 export const radToDeg = (radians: number) => (radians * 180) / Math.PI;
+export const lerp = (start: number, end: number, amount: number) => {
+  return (1 - amount) * start + amount * end;
+};
